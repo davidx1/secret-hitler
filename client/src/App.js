95c45ca87a3f6c79a888521c45b30d81db9dade1
David Xu <davidxu1991@gmail.com>
2020-03-31 23:51:03 +1300

    successfully communicate between server and client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import * as Colyseus from "colyseus.js";
+
+const host = window.document.location.host.replace(/:.*/, "");
+const location = window.location;
+const client = new Colyseus.Client(
+  location.protocol.replace("http", "ws") +
+    "//" +
+    host +
+    (location.port ? ":" + 3001 : "")
+);
+
+const useRoom = () => {
+  const [room, setRoom] = useState();
+
+  useEffect(
+    () =>
+      client.joinOrCreate("my_room").then(newRoom => {
+        newRoom.onMessage(function(message) {
+          console.log(message);
+        });
+
+        setRoom(newRoom);
+      }),
+    []
+  );
+
+  return { room };
+};
 
 function App() {
+  const { room } = useRoom();
+
+  const onClick = () => {
+    room.send({ move: "left" });
+  };
+
+  const onClick2 = () => {
+    room.send({ move: "right" });
+  };
+
   return (
     <div>
-      <input />
+      <button onClick={onClick}>Button</button>
+      <button onClick={onClick2}>Button</button>
       <p>Hello world</p>
     </div>
   );
--- src/MyRoom.ts
+++ src/MyRoom.ts
@@ -3,18 +3,24 @@ import { Room, Client } from "colyseus";
 export class MyRoom extends Room {
 
   onCreate (options: any) {
-  }
+    console.log("BasicRoom created!", options);
+}
 
-  onJoin (client: Client, options: any) {
-  }
+onJoin (client: { sessionId: any; }) {
+    this.broadcast(`${ client.sessionId } joined.`);
+}
 
-  onMessage (client: Client, message: any) {
-  }
+onLeave (client: { sessionId: any; }) {
+    this.broadcast(`${ client.sessionId } left.`);
+}
 
-  onLeave (client: Client, consented: boolean) {
-  }
+onMessage (client: { sessionId: any; }, data: { message: any; }) {
+    console.log("BasicRoom received message from", client.sessionId, ":", data);
+    this.broadcast(`(${ client.sessionId }) ${ data.message }`);
+}
 
-  onDispose() {
-  }
+onDispose () {
+    console.log("Dispose BasicRoom");
+}
 
 }
